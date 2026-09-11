abd7fcd0468751a6b2363e1b5276418abb2610d4
wenq705-sys <wenq705@gmail.com>
2026-09-11 21:06:27 +1000

    feat: export client prediction helper

--- shared/index.ts
+++ shared/index.ts
@@ -1,5 +1,7 @@
 export { Board2048 } from './game/Board2048';
 export { SeededRandom, randomUint32 } from './game/SeededRandom';
+export { predictMoveTiles } from './game/predictMove';
+export type { PredictedMotion, PredictedMove } from './game/predictMove';
 export type { RandomSource } from './game/SeededRandom';
 export type {
   BoardPublicState,
