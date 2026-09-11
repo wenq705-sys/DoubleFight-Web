7df84da9b84449677e0f7b447fb5b347d40eb3da
wenq705-sys <wenq705@gmail.com>
2026-09-11 20:34:59 +1000

    feat: add shared core public exports

---
+++ shared/index.ts
@@ -0,0 +1,29 @@
+export { Board2048 } from './game/Board2048';
+export { SeededRandom, randomUint32 } from './game/SeededRandom';
+export type { RandomSource } from './game/SeededRandom';
+export type {
+  BoardPublicState,
+  BoardTile,
+  CellPosition,
+  ClearResult,
+  Direction,
+  MergeEvent,
+  MoveResult,
+  TileMotion,
+} from './game/types';
+export {
+  PROTOCOL_VERSION,
+  isDirection,
+  isNetworkThemeId,
+  parseClientMessage,
+} from './protocol/messages';
+export type {
+  ClientMessage,
+  MatchPlayerState,
+  MatchSnapshot,
+  NetworkThemeId,
+  RoomPhase,
+  RoomPlayerState,
+  RoomState,
+  ServerMessage,
+} from './protocol/messages';
