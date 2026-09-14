a15aa1359e7130ac8c946b224d05db61f5050e69
wenq705-sys <wenq705@gmail.com>
2026-09-14 20:55:31 +1000

    feat: add Douyin product configuration

---
+++ platform/douyin/src/config.ts
@@ -0,0 +1,16 @@
+export const DOUYIN_PRODUCT_CONFIG = {
+  appId: 'tta405d2b83b08e6f207',
+  socketUrl: 'wss://game.whvwayfare.online/ws',
+  ads: {
+    banner: 'c4mdhkf94nebkcc1dk',
+    rewarded: 'kah48dcjfih55jh1ga',
+    interstitial: '4dkwv7i3rr7q6dq71n',
+  },
+  share: {
+    templateId: '',
+  },
+  ranking: {
+    soloZone: 'solo',
+    pvpZone: 'pvp',
+  },
+} as const;
